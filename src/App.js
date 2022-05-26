963e5a165c7fc2bd0840e10eeadd03629aae624b
INO <bih0905@naver.com>
2022-05-26 20:51:39 +0900

    220526

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import ThankyouDialog from './components/3-6.Composition2/ThankyouDialog';
+// import Dialog from './components/3-6.Composition2/Dialog';
+// import WelcomeDialog from './components/3-5.Composition/WelcomeDialog';
+// import Reducer from './components/3-3.Hooks/Reducer';
+// import State from './components/3-3.Hooks/State';
+// import List from './components/2-9.List/List';
+// import ControlledComponent from './components/2-10.Form/ControlledComponent';
+// import UncontrolledComponent from './components/2-10.Form/UncontrolledComponent';
+// import Condition from './components/2-8.ConditionalRendering/Condition';
+// import Event from './components/2-7.Event/Event';
+// import Composition from './components/2-4.Props/Composition';
+// import Extraction from './components/2-4.Props/Extraction/Extraction';
+// import ClassComponent from './components/2-5.State/ClassComponent';
+// import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+// import ClassComponent2 from './components/2-6.LifeCycle/ClassComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThankyouDialog />
+      {/* <Dialog />
+      <WelcomeDialog />
+      <Reducer />
+      <State />
+      <ControlledComponent />
+      <UncontrolledComponent />
+      <List />
+      <Condition />
+      <Event />
+      <ClassComponent2 />
+      <FunctionalComponent />
+      <ClassComponent />
+      <Composition />
+      <Extraction /> */}
     </div>
   );
 }
